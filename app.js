ba8eb78d14e0538502a595209340e1c6cde33be6
Julio Silva <julioinfo21@yahoo.com>
2023-10-10 21:49:47 -0300

    Atualizando limite

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaNumerosSorteados = [];
-let limiteLista = 10;
+let limiteLista = 50;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -11,7 +11,7 @@ function exibirTextoNaTela(tag, texto){
 
 function mensagemReiniciarJogo(){
     exibirTextoNaTela('h1',  'Hora do Desafio');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 a 10');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 a 50');
 }
 
 mensagemReiniciarJogo();
